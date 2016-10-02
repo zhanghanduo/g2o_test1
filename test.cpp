80f0394355a0b63764fe53c353c05ab3de349339
Jeffsan <jeffsan.wang@gmail.com>
2016-10-02 21:19:27 +0800

    test branch

--- test.cpp
+++ test.cpp
@@ -10,71 +10,87 @@
 #include <g2o/core/robust_kernel.h>
 #include <g2o/core/robust_kernel_impl.h>
 #include <g2o/core/optimization_algorithm_levenberg.h>
+#include <g2o/core/optimization_algorithm_gauss_newton.h>
 #include <g2o/solvers/cholmod/linear_solver_cholmod.h>
+#include <g2o/solvers/csparse/linear_solver_csparse.h>
+#include <g2o/solvers/dense/linear_solver_dense.h>
+
 #include <g2o/types/slam3d/se3quat.h>
 #include <g2o/types/sba/types_six_dof_expmap.h>
-
+#include <g2o/types/slam3d/vertex_se3.h>
+#include <g2o/types/slam3d/edge_se3.h>
 using namespace std;
-
+using namespace g2o;
+//using namespace g2o::types_six_dof_expmap;
 
 int main( int argc, char** argv )
 {
     g2o::SparseOptimizer    optimizer;
-    g2o::BlockSolver_6_3::LinearSolverType* linearSolver = new  g2o::LinearSolverCholmod<g2o::BlockSolver_6_3::PoseMatrixType> ();
+    g2o::BlockSolver_6_3::LinearSolverType* linearSolver = new  g2o::LinearSolverDense<g2o::BlockSolver_6_3::PoseMatrixType> ();
     g2o::BlockSolver_6_3* block_solver = new g2o::BlockSolver_6_3( linearSolver );
-    g2o::OptimizationAlgorithmLevenberg* algorithm = new g2o::OptimizationAlgorithmLevenberg( block_solver );
+    g2o::OptimizationAlgorithmGaussNewton*  algorithm = new g2o::OptimizationAlgorithmGaussNewton( block_solver );
     optimizer.setAlgorithm( algorithm );
     optimizer.setVerbose( true );
 
     size_t num = 4;
     for (int i = 0; i < num; i++)
     {
-        g2o::VertexSE3Expmap* v = new g2o::VertexSE3Expmap();
+        VertexSE3* v = new VertexSE3();
         v->setId(i);
         if ( i == 0)
             v->setFixed( true );
-        v->setEstimate( g2o::SE3Quat() );
+        v->setEstimate( g2o::Isometry3D::Identity() );
         optimizer.addVertex( v );
     }
 
-    vector<g2o::EdgeSE3Expmap *> edges;
+    vector<g2o::EdgeSE3 *> edges;
     for (size_t i = 0; i < num; ++i)
     {
-        g2o::EdgeSE3Expmap *edge = new g2o::EdgeSE3Expmap();
-        edge->setVertex(0, dynamic_cast<g2o::VertexSE3Expmap *>   (optimizer.vertex(i)));
-        edge->setVertex(1, dynamic_cast<g2o::VertexSE3Expmap *>   (optimizer.vertex((i + 1) % num)));
+        g2o::EdgeSE3 *edge = new g2o::EdgeSE3();
+        edge->setVertex(0, dynamic_cast<g2o::VertexSE3 *>   (optimizer.vertex(i)));
+        edge->setVertex(1, dynamic_cast<g2o::VertexSE3 *>   (optimizer.vertex((i + 1) % num)));
 
-        g2o::SE3Quat *pose = new g2o::SE3Quat();
-        pose->setRotation(Eigen::Quaterniond::Identity());
-        switch (i % num) {
+//        Isometry3D
+        g2o::Isometry3D *pose = new g2o::Isometry3D();
+        pose->setIdentity();
+//        pose->rotation() = Eigen::Matrix3d::Identity();
+        switch (i % num)
+        {
             case 0:
-                pose->setTranslation(g2o::Vector3D(1, 0, 0) + 0.1 * g2o::Vector3D::Random());
+                pose->translation() = g2o::Vector3D(1, 0, 0) + 0.1 * g2o::Vector3D::Random();
+//                pose->setTranslation(g2o::Vector3D(1, 0, 0) + 0.1 * g2o::Vector3D::Random());
 //                cout <<i<<" "<<pose->toVector()<<endl;
                 break;
             case 1:
-                pose->setTranslation(g2o::Vector3D(0, 1, 0) + 0.1 * g2o::Vector3D::Random());
+                pose->translation() = g2o::Vector3D(0, 1, 0) + 0.1 * g2o::Vector3D::Random();
+
+//                pose->setTranslation(g2o::Vector3D(0, 1, 0) + 0.1 * g2o::Vector3D::Random());
                 break;
             case 2:
-                pose->setTranslation(g2o::Vector3D(-1, 0, 0) + 0.1 * g2o::Vector3D::Random());
+                pose->translation() = g2o::Vector3D(-1, 0, 0) + 0.1 * g2o::Vector3D::Random();
+
+//                pose->setTranslation(g2o::Vector3D(-1, 0, 0) + 0.1 * g2o::Vector3D::Random());
                 break;
             case 3:
-                pose->setTranslation(g2o::Vector3D(0, -1, 0) + 0.1 * g2o::Vector3D::Random());
+                pose->translation() = g2o::Vector3D(0, -1, 0) + 0.1 * g2o::Vector3D::Random();
+
+//                pose->setTranslation(g2o::Vector3D(0, -1, 0) + 0.1 * g2o::Vector3D::Random());
                 break;
             case 4:
-                pose->setTranslation(g2o::Vector3D(-1, 1, 0) + 0.1 * g2o::Vector3D::Random());
+                pose->translation() = g2o::Vector3D(-1, 1, 0) + 0.1 * g2o::Vector3D::Random();
                 edge->setVertex(0, dynamic_cast<g2o::VertexSE3Expmap *>   (optimizer.vertex(1)));
                 edge->setVertex(1, dynamic_cast<g2o::VertexSE3Expmap *>   (optimizer.vertex(3)));
                 break;
             case 5:
-                pose->setTranslation(g2o::Vector3D(1, 1, 0) + 0.1 * g2o::Vector3D::Random());
+                pose->translation() = g2o::Vector3D(1, 1, 0) + 0.1 * g2o::Vector3D::Random();
                 edge->setVertex(0, dynamic_cast<g2o::VertexSE3Expmap *>   (optimizer.vertex(0)));
                 edge->setVertex(1, dynamic_cast<g2o::VertexSE3Expmap *>   (optimizer.vertex(2)));
         }
-        cout << "measurement id " << i << " Pose=" << endl << pose->toVector().transpose() << endl;
+        cout << "measurement id " << i << " Pose=" << endl << pose->matrix() << endl;
         edge->setMeasurement(*pose);
-        edge->setInformation(Eigen::MatrixXd::Identity(6, 6));
+        edge->setInformation(Eigen::MatrixXd::Identity(6, 6)*0.01);
         edge->setParameterId(0, 0);
-        edge->setRobustKernel( new g2o::RobustKernelHuber() );
+        edge->setRobustKernel( new g2o::RobustKernelTukey() );
         optimizer.addEdge( edge );
         edges.push_back(edge);
     }
@@ -87,7 +103,7 @@ int main( int argc, char** argv )
 
     for (size_t i = 0; i < edges.size(); i++)
     {
-        g2o::VertexSE3Expmap *v = dynamic_cast<g2o::VertexSE3Expmap *> (optimizer.vertex(i));
+        g2o::VertexSE3 *v = dynamic_cast<g2o::VertexSE3 *> (optimizer.vertex(i));
         Eigen::Isometry3d pos = v->estimate();
         cout << "vertex id " << i << " Pose=" << endl << pos.matrix() << endl;
     }
@@ -102,6 +118,7 @@ int main( int argc, char** argv )
         }
         else
         {
+            cout<<"error = "<<e->chi2()<<endl;
             inliers++;
         }
     }
