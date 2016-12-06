a71426e94cd004df11dc45fb0f779ec83f4dc732
christlurker <hzhang032@e.ntu.edu.sg>
2016-12-06 20:45:08 +0800

    begin testing pure translation estimation

---
+++ orb.cpp
@@ -0,0 +1,102 @@
+// This example consists of a single static target which sits in one
+// place and does not move; in effect it has a "GPS" which measures
+// its position
+
+#include <Eigen/StdVector>
+#include <iostream>
+#include <stdint.h>
+
+#include <g2o/core/sparse_optimizer.h>
+#include <g2o/core/block_solver.h>
+#include <g2o/core/solver.h>
+#include <g2o/core/optimization_algorithm_gauss_newton.h>
+#include <g2o/solvers/cholmod/linear_solver_cholmod.h>
+#include <g2o/stuff/sampler.h>
+#include "g2o/solvers/dense/linear_solver_dense.h"
+#include "targetTypes3D.hpp"
+#include "g2o/types/sba/types_six_dof_expmap.h"
+
+using namespace Eigen;
+using namespace std;
+using namespace g2o;
+
+int main()
+{
+  // Set up the optimiser
+  SparseOptimizer optimizer;
+  optimizer.setVerbose(false);
+
+  // Create the block solver - the dimensions are specified because
+  // 3D observations marginalise to a 3D estimate
+//  typedef BlockSolver<BlockSolverTraits<3, 3> > BlockSolver_3_3;
+  g2o::BlockSolver_3_3::LinearSolverType* linearSolver
+          = new LinearSolverDense<BlockSolver_3_3::PoseMatrixType>();
+//      = new LinearSolverCholmod<BlockSolver_3_3::PoseMatrixType>();
+  BlockSolver_3_3* blockSolver
+      = new BlockSolver_3_3(linearSolver);
+  OptimizationAlgorithmGaussNewton* solver
+    = new OptimizationAlgorithmGaussNewton(blockSolver);
+  optimizer.setAlgorithm(solver);
+
+  // Sample the actual location of the target
+  vector<Vector3d> truePoint；
+  // Vector3d truePoint1(5,3.2,100);
+  // Vector3d truePoint2(-7.5,10,58);
+  // Vector3d truePoint3(8,-9,87);
+
+  // Construct vertex which corresponds to the actual point of the target
+  VertexSBAPointXYZ * Trans3 = new VertexSBAPointXYZ();
+  Trans3->setEstimate(0,0,14);
+  Trans3->setId(0);
+  Trans3->setFixed(false);
+  optimizer.addVertex(Trans3);
+
+  // Now generate some noise corrupted measurements; for simplicity
+  // these are uniformly distributed about the true target. These are
+  // modelled as a unary edge because they do not like to, say,
+  // another node in the map.
+  int numMeasurements = 4;
+  double noiseLimit = sqrt(12.);
+  double noiseSigma = noiseLimit*noiseLimit / 12.0;
+
+  for (int i = 0; i < numMeasurements; i++)
+    {
+      Matrix<double,2,1> obs;
+      obs << 
+      Vector3d measurement = truePoint +
+        Vector3d(sampleUniform(-0.5, 0.5) * noiseLimit,
+                 sampleUniform(-0.5, 0.5) * noiseLimit,
+                 sampleUniform(-0.5, 0.5) * noiseLimit);
+      GPSObservationPosition3DEdge* goe = new GPSObservationPosition3DEdge();
+      goe->setVertex(0, position);
+      goe->setMeasurement(measurement);
+      goe->setInformation(Matrix3d::Identity() / noiseSigma);
+      optimizer.addEdge(goe);
+    }
+
+  // Configure and set things going
+  optimizer.initializeOptimization();
+  optimizer.setVerbose(true);
+  optimizer.optimize(5);
+
+  cout << "truePoint=\n" << truePoint << endl;
+
+  cerr <<  "computed estimate=\n" << dynamic_cast<VertexPosition3D*>(optimizer.vertices().find(0)->second)->estimate() << endl;
+
+  //position->setMarginalized(true);
+
+  SparseBlockMatrix<MatrixXd> spinv;
+
+  optimizer.computeMarginals(spinv, position);
+
+
+
+  //optimizer.solver()->computeMarginals();
+
+  // covariance
+  //
+  cout << "covariance\n" << spinv << endl;
+
+  cout << spinv.block(0,0) << endl;
+
+}
